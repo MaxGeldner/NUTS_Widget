5c822360a764a1a9850904d4967b8cb1a0cc0d0c
Max Geldner <max.sgeldner@t-online.de>
2022-07-03 11:27:26 +0200

    - Moved tracking to own component

--- App/App.tsx
+++ App/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import { useEffect } from 'react';
-
-import * as Location from 'expo-location';
-import * as TaskManager from 'expo-task-manager';
-import { LocationObjectCoords } from 'expo-location';
-import { NUTSData, NUTSLevelObject } from './types/NUTS';
-
-import Geometry from './helper/Geometry';
+import { NUTSLevelObject } from './types/NUTS';
 
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, /* NativeModules, */ Dimensions } from 'react-native';
+import { StyleSheet, Text, /* NativeModules, */ Dimensions } from 'react-native';
 import LocationDisplay from './components/LocationDisplay';
+import TrackingButton from './components/TrackingButton';
 import { NativeBaseProvider, Center, Box } from "native-base";
 
 const NUTS_DATA = require('./assets/NUTS_ALL_DE.json');
@@ -18,7 +12,6 @@ const NUTS_DATA = require('./assets/NUTS_ALL_DE.json');
 // const SharedStorage = NativeModules.SharedStorage;
 
 export default function App() {
-  const TRACKING_TASK_NAME = 'location_tracking'
   let locationLevels: NUTSLevelObject = {
     0: '-',
     1: '-',
@@ -31,111 +24,28 @@ export default function App() {
   let [trackingTime, setTrackingTime] = React.useState(new Date());
   let [trackingStarted, setTrackingStarted] = React.useState(false);
 
-  useEffect(() => {
-    onAppStart();
-  }, []);
-
-  const onAppStart = async () => {
-    await requestLocationPermissions();
+  const onTrackStart = () => {
+    setTrackingStarted(true);
   }
 
-  const defineTrackingTask = () => {
-    TaskManager.defineTask(TRACKING_TASK_NAME, ({ data, error }: { data: any, error: any }) => {
-      const locations: Array<{coords: LocationObjectCoords}> = data.locations;
-      if (error) {
-        console.error(error);
-        return;
-      }
-      const lat = locations[0]?.coords?.latitude;
-      const lng = locations[0]?.coords?.longitude;
+  const onTrack = (location: NUTSLevelObject) => {
+    setLocation((prevLocation) => ({ ...prevLocation, ...location }));
+    setTrackingCount((prevTrackingCount) => prevTrackingCount + 1);
+    setTrackingTime(() => new Date());
 
-      let found: NUTSLevelObject = {
-        0: false,
-        1: false,
-        2: false,
-        3: false,
-      };
-      NUTS_DATA.features.forEach((feature: NUTSData) => {
-        if (Object.values(found).every(lvl => lvl)) {
-          return;
-        }
-
-        if (feature.geometry.type === 'Polygon') {
-          const polygon = feature.geometry.coordinates[0].map((c: Array<number>) => ({ x: c[0], y: c[1] }));
-          if (Geometry.pointInPolygon({ x: lng, y: lat }, polygon)) {
-            locationLevels[feature.properties.LEVL_CODE] = feature.properties.NAME_LATN
-            found[feature.properties.LEVL_CODE] = true;
-            return;
-          }
-        } else if (feature.geometry.type === 'MultiPolygon') {
-          const multiPolygon = feature.geometry.coordinates.map((poly: Array<Array<Array<number>>>) => poly[0].map((c: Array<number>) => ({ x: c[1], y: c[0] })));
-          if (Geometry.pointInMultiPolygon({ x: lat, y: lng }, multiPolygon)) {
-            locationLevels[feature.properties.LEVL_CODE] = feature.properties.NAME_LATN
-            found[feature.properties.LEVL_CODE] = true;
-            return;
-          }
-        }
-      });
-
-      setLocation((prevLocation) => ({ ...prevLocation, ...locationLevels }));
-      setTrackingCount((prevTrackingCount) => prevTrackingCount + 1);
-      setTrackingTime(() => new Date());
-
-      // SharedStorage.set(JSON.stringify({locationLevels}));
-     }
-    );
+    // SharedStorage.set(JSON.stringify({locationLevels}));
   }
 
-  const startTracking = async () => {
-    defineTrackingTask();
-
-    await Location.startLocationUpdatesAsync(TRACKING_TASK_NAME, {
-      accuracy: Location.Accuracy.BestForNavigation,
-      timeInterval: 10000,
-      showsBackgroundLocationIndicator: true,
-      deferredUpdatesInterval: 100
-    });
-
-    const hasStarted = await Location.hasStartedLocationUpdatesAsync(TRACKING_TASK_NAME);
-    setTrackingStarted(hasStarted);
-  };
-
-  const requestLocationPermissions = async () => {
-    let foregroundPerm = await Location.requestForegroundPermissionsAsync();
-    let backgroundPerm = await Location.requestBackgroundPermissionsAsync();
-    if (foregroundPerm.status != 'granted' && backgroundPerm.status !== 'granted') {
-        console.log('Permission to access location was denied');
-    } else {
-        console.log('Permission to access location granted');
-    }
-  };
-
-  const handlePressStartTracking = () => {
-    startTracking();
-  };
-
-  const handlePressStopTracking = () => {
+  const onTrackStop = () => {
     setTrackingStarted(false);
-
-    TaskManager.isTaskRegisteredAsync(TRACKING_TASK_NAME)
-      .then((tracking) => {
-        if (tracking) {
-          Location.stopLocationUpdatesAsync(TRACKING_TASK_NAME);
-        }
-      });
-  };
+  }
 
   return (
     <NativeBaseProvider>
       <Box safeArea style={styles.container}>
         <Center style={styles.heading}><Text style={styles.headingText}>Wo bin ich?</Text></Center>
         <Center style={styles.trackingButton}>
-          {
-          trackingStarted ?
-            <Button title="Tracking stoppen" onPress={handlePressStopTracking} color="#ff000099" />
-            :
-            <Button title="Tracking starten" onPress={handlePressStartTracking} color="#0000ff99" />
-          }
+          <TrackingButton onTrackStart={onTrackStart} onTrack={onTrack} onTrackStop={onTrackStop} nutsData={NUTS_DATA} />
         </Center>
         <Center>
           <LocationDisplay location={location} />
---
+++ App/components/TrackingButton.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { Button } from 'react-native';
+
+import * as Location from 'expo-location';
+import * as TaskManager from 'expo-task-manager';
+import { LocationObjectCoords } from 'expo-location';
+import { NUTSData, NUTSLevelObject } from '../types/NUTS';
+
+import Geometry from '../helper/Geometry';
+
+export default function TrackingButton ({ onTrackStart, onTrackStop, onTrack, nutsData }: { onTrackStart: Function, onTrackStop: Function, onTrack: Function, nutsData: any }) {
+    const TRACKING_TASK_NAME = 'location_tracking'
+    let locationLevels: NUTSLevelObject = {
+      0: '-',
+      1: '-',
+      2: '-',
+      3: '-',
+    };
+
+    let [trackingStarted, setTrackingStarted] = React.useState(false);
+
+    const requestLocationPermissions = async () => {
+        let foregroundPerm = await Location.requestForegroundPermissionsAsync();
+        let backgroundPerm = await Location.requestBackgroundPermissionsAsync();
+        if (foregroundPerm.status != 'granted' && backgroundPerm.status !== 'granted') {
+            console.log('Permission to access location was denied');
+        } else {
+            console.log('Permission to access location granted');
+        }
+    };
+  
+    const defineTrackingTask = () => {
+      TaskManager.defineTask(TRACKING_TASK_NAME, ({ data, error }: { data: any, error: any }) => {
+        const locations: Array<{coords: LocationObjectCoords}> = data.locations;
+        if (error) {
+          console.error(error);
+          return;
+        }
+        const lat = locations[0]?.coords?.latitude;
+        const lng = locations[0]?.coords?.longitude;
+  
+        let found: NUTSLevelObject = {
+          0: false,
+          1: false,
+          2: false,
+          3: false,
+        };
+        nutsData.features.forEach((feature: NUTSData) => {
+          if (Object.values(found).every(lvl => lvl)) {
+            return;
+          }
+  
+          if (feature.geometry.type === 'Polygon') {
+            const polygon = feature.geometry.coordinates[0].map((c: Array<number>) => ({ x: c[0], y: c[1] }));
+            if (Geometry.pointInPolygon({ x: lng, y: lat }, polygon)) {
+              locationLevels[feature.properties.LEVL_CODE] = feature.properties.NAME_LATN
+              found[feature.properties.LEVL_CODE] = true;
+              return;
+            }
+          } else if (feature.geometry.type === 'MultiPolygon') {
+            const multiPolygon = feature.geometry.coordinates.map((poly: Array<Array<Array<number>>>) => poly[0].map((c: Array<number>) => ({ x: c[1], y: c[0] })));
+            if (Geometry.pointInMultiPolygon({ x: lat, y: lng }, multiPolygon)) {
+              locationLevels[feature.properties.LEVL_CODE] = feature.properties.NAME_LATN
+              found[feature.properties.LEVL_CODE] = true;
+              return;
+            }
+          }
+        });
+
+        onTrack(locationLevels);
+       }
+      );
+    }
+  
+    const startTracking = async () => {
+      await requestLocationPermissions();
+
+      defineTrackingTask();
+  
+      await Location.startLocationUpdatesAsync(TRACKING_TASK_NAME, {
+        accuracy: Location.Accuracy.BestForNavigation,
+        timeInterval: 10000,
+        showsBackgroundLocationIndicator: true,
+        deferredUpdatesInterval: 100
+      });
+  
+      const hasStarted = await Location.hasStartedLocationUpdatesAsync(TRACKING_TASK_NAME);
+      hasStarted && onTrackStart();
+      setTrackingStarted(hasStarted);
+    };
+
+    const stopTracking = () => {
+        TaskManager.isTaskRegisteredAsync(TRACKING_TASK_NAME)
+        .then((tracking) => {
+          if (tracking) {
+            Location.stopLocationUpdatesAsync(TRACKING_TASK_NAME);
+            onTrackStop();
+            setTrackingStarted(false);
+          }
+        });
+    }
+  
+    const handlePressStartTracking = () => {
+      startTracking();
+    };
+  
+    const handlePressStopTracking = () => {
+      stopTracking();
+    };
+
+    return trackingStarted ? 
+        <Button title="Tracking stoppen" onPress={handlePressStopTracking} color="#ff000099" />
+        : <Button title="Tracking starten" onPress={handlePressStartTracking} color="#0000ff99" />
+}
